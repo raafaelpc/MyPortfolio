d4287e09f12e0678143271d3dab4f294475ae8e0
Rafael Patricio <rafael29pc@hotmail.com>
2022-03-23 10:58:56 -0300

    PortfolioV2

---
+++ src/resumeData.js
@@ -0,0 +1,120 @@
+let resumeData = {
+    "name": "Rafael",
+    "role": "",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Estudante de Ciências da Computação empenhado no estudo e prática do Desenvolvimento Web.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rafael-patricio-305b191a2/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/raafaelpc",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"http://instagram.com/raafaelpc",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"Meu nome é Rafael Patricio, tenho 21 anos, estou cursando o 5º período no curso de Ciências da Computação pelo Centro Universitário Estácio do Ceará - Campus Moreira Campos. Previsão de termino do curso em dez/23. Eu sou muito entusiasta pelo desenvolvimento Web e estou a procura de uma oportunidade para ganhar experiência e melhorar meu desempenho.",
+    "address":"",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Colégio Monsenhor Joviniano Barreto",
+        "specialization":"Ensino Médio",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2017",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Centro Universitário Estácio do Ceará - Campus Moreira Campos",
+        "specialization":"Graduação em andamento",
+        "MonthOfPassing":"Ciências da Computação",
+        "YearOfPassing":"",
+        "Achievements":"Previsão de termino - Dez/2023"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"ReactJS"
+      },
+      {
+        "skillname":"Php"
+      },
+      {
+        "skillname":"Mysql"
+      },
+      {
+        "skillname":"Angular"
+      },
+      {
+        "skillname":"Java"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
